869f71cffffcac0c5ce9525e36b917f993dc82cb
vessipetrova <135272277+vessipetrova@users.noreply.github.com>
2025-12-16 11:51:49 +0200

    Feature: Add Reset UI Button
    feat: added reset button
    
    feat: style reset button
    
    feat: add reset button logic

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ document.getElementById('apply-btn').addEventListener('click', function () {
   var c = document.getElementById('color-input').value;
   document.getElementById('box').style.backgroundColor = c;
 });
+
+document.getElementById('reset-btn').addEventListener('click', function () {
+  document.getElementById('box').style.backgroundColor = '';
+  document.getElementById('color-input').value = '';
+});
